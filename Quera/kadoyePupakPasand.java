51f655ea3f4f61e248b6358c56b049793933bc81
Mohammad Salek <salek.mohmd@gmail.com>
2018-02-24 20:37:23 +0330

    Create kadoyePupakPasand.java

---
+++ Quera/kadoyePupakPasand.java
@@ -0,0 +1,15 @@
+// This is one of the most ridiculous submits... and it got accepted!
+// https://quera.ir/course/assignments/4495/problems/14995
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args){
+       Scanner sc = new Scanner(System.in);  
+       
+       BigInteger num = sc.nextBigInteger();
+       
+       System.out.println(num);
+    }
+}
