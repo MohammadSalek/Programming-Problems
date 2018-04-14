39e21369208cf8dd9f25268b520e2495712e7951
Mohammad Salek <salek.mohmd@gmail.com>
2018-04-14 16:25:56 +0430

    Create J_niceTryButTimeLimit.java

---
+++ TabrizU-ACM-ICPC/PracticePart1/J_niceTryButTimeLimit.java
@@ -0,0 +1,54 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = Integer.parseInt(sc.nextLine());
+		BigInteger bigNum;
+		
+		for(int i=0; i<num; i++) {
+			bigNum = new BigInteger(sc.nextLine());
+			boolean beautyFlag = false;			
+			while(!beautyFlag) {
+				bigNum = bigNum.subtract(BigInteger.ONE);
+				int digits = countDigits(bigNum);
+				if(digits%2 == 1) {
+					bigNum = makeItEven(bigNum, digits);
+				}				
+				beautyFlag = isBeautiful(bigNum);				
+			}
+			System.out.println(bigNum);
+		}		
+	}
+
+	static BigInteger makeItEven(BigInteger num, int digits) {	
+		char[] array = new char[digits-1];
+		for(int i=0; i<digits-1; i++) {
+			array[i] = '9';
+		}
+		return new BigInteger(new String(array));
+	}
+
+	static boolean isBeautiful(BigInteger num) {				
+		int[] numbers = new int[10];
+		char[] numStr = (num.toString()).toCharArray() ;				
+		int counter = numStr.length - 1;
+		
+		while(counter >= 0) {							
+			numbers[(int) numStr[counter]-'0']++;
+			counter--;				
+		}
+		for (int i = 0; i < 10; i++) {
+			if(numbers[i]%2 != 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	static int countDigits(BigInteger number) {
+		  return number.toString().length();
+	}	
+}
